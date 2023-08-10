b777f4036cf3be3a0691427e0de38fde28eee10c
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2023-08-10 20:19:56 -0300

    feat(Global): Início do website
    Criei a base do projeto.

---
+++ src/main/java/com/grupoCampanha/grupoCampanha/GrupoCampanhaApplication.java
@@ -0,0 +1,13 @@
+package com.grupoCampanha.grupoCampanha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupoCampanhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupoCampanhaApplication.class, args);
+	}
+
+}
